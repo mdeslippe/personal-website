07f89b1440e0a1aebe260dcabbf579c4defea7d4
mdeslippe <mdeslippe76@gmail.com>
2021-10-12 20:41:02 -0500

    Minor refactor

--- js/scroll.js
+++ js/scroll.js
@@ -15,6 +15,18 @@ let time = Date.now();
 
 function scrollEventThrottle(event) {
 
+  // If the user is trying to resize their screen, ignore the event.
+  if(event.ctrlKey)
+    return;
+
+  // Smooth scrolling doesn't work on firefox.
+  if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1)
+    return;
+
+  // Mobile looks better with manual scrolling.
+  if (window.innerWidth <= 800)
+    return;
+
   event.preventDefault();
 
   if ((time + 500 - Date.now()) < 0) {
